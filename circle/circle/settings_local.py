63d89f4fbd9567273e021c5453aeca4cb2a6ec42
Brian Wiborg <baccenfutter@c-base.org>
2014-10-11 19:35:53 +0200

    fix project directory structure; closes #4

---
+++ circle/circle/settings_local.py
@@ -0,0 +1,38 @@
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+SECRET_KEY = '+6x40s^(7zq=-j@^8c*h$dlwwxyqk1l5^+4(9i-cw!=^mq(%wp'
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'circleapp',  # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'circleapp',
+        'PASSWORD': 'circleapp',
+        'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',  # Set to empty string for default.
+    }
+}
+
+TIME_ZONE = 'Europe/Berlin'
+
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+MEDIA_ROOT = ''
+MEDIA_URL = ''
+
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    '/home/smile/circle/static',
+)
+
