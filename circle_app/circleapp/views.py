898fd52d4cffdd636f1029a9940eb5f6b038f4b0
smile <smile@c-base.org>
2014-11-02 00:56:23 +0100

    views for angular and topic added

--- circle_app/circleapp/views.py
+++ circle_app/circleapp/views.py
@@ -11,12 +11,14 @@ from django.template.context import RequestContext
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.views.generic import FormView, TemplateView
+from django.core.urlresolvers import reverse_lazy
 
 #from django.contrib.auth.models import User
 from django.contrib.sessions.models import Session
 from datetime import datetime
 
-from forms import LoginForm
+from forms import LoginForm, MemberForm
 
 from jsonrpc import jsonrpc_method
 from rest_framework import viewsets
@@ -73,6 +75,17 @@ def show_topic(request, topic_uuid):
 def topic_pad(request, topic_uuid):
     return HttpResponseRedirect(Topic.objects.get(uuid=topic_uuid).etherpad_link)
 
+def add_topic(request):
+    return render(request, 'edit_topic.django', {})
+
+@login_required
+def select_user(request):
+    return render(request, 'select_user.django', {})
+
+@login_required
+def add_user(request):
+    return render(request, 'select_user.django', {})
+
 def auth_login(request):
     redirect_to = request.REQUEST.get('next', '') or '/'
     if request.method == 'POST':
@@ -89,8 +102,6 @@ def auth_login(request):
         form = LoginForm()
     return render_to_response('login.django', RequestContext(request,
                                                              locals()))
-
-
 def auth_logout(request):
     redirect_to = request.REQUEST.get('next', '') or '/'
     logout_auth(request)
@@ -135,7 +146,6 @@ circle_detail = CircleViewSet.as_view({
     'delete': 'destroy'
 })
 
-
 class CurrentCircleViewSet(viewsets.ModelViewSet):
     """
     Viewset for api/circles/current/
@@ -156,10 +166,11 @@ current_circle_detail = CurrentCircleViewSet.as_view({
 
 class TopicViewSet(viewsets.ModelViewSet):
     """
-    Viewset for api/topic/:id/
+    Viewset for api/topic/:uuid/
     """
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
+    lookup_field = 'uuid'
 
 topic_list = snippet_list = TopicViewSet.as_view({
     'get': 'list',
@@ -173,4 +184,25 @@ topic_detail = TopicViewSet.as_view({
     'delete': 'destroy'
 })
 
+class AjaxTemplateMixin(object):
+    def dispatch(self, request, *args, **kwargs):
+        if not hasattr(self, 'ajax_template_name'):
+            split = self.template_name.split('.html')
+            split[-1] = '_inner'
+            split.append('.html')
+            self.ajax_template_name = ''.join(split)
+        if request.is_ajax():
+            self.template_name = self.ajax_template_name
+        return super(AjaxTemplateMixin, self).dispatch(request, *args, **kwargs)
+
+class AddUserFormView(AjaxTemplateMixin, FormView):
+    template_name = 'select_user.django'
+    ajax_template_name = "select_user.django"
+    form_class = MemberForm
+    success_url = reverse_lazy('home')
+    success_message = "Way to go!"
+
+#https://github.com/d-m/django-modal-forms/blob/master/test_app/urls.py
+#class HomeView(TemplateView):
+    #template_name = 'test_app/home.html'
 
